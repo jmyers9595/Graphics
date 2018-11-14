3ab94c65f97b19bfa21a54354bd9572e657df50a
Jason Myers <Jason.myers-1@ou.edu>
2018-11-14 14:25:21 -0600

    first commit

---
+++ eclipse-workspace/Graphics/src/MyFrame.java
@@ -0,0 +1,23 @@
+import javax.swing.JFrame;
+public class MyFrame extends JFrame
+{
+    private static final int HEIGHT = 400;
+    private static final int WIDTH = 300;
+    private MyPanel panel;
+    
+    public MyFrame(String title)
+    {
+        setTitle(title);
+        setSize(WIDTH,HEIGHT);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        
+    }
+
+    public static void main(String[] args)
+    {
+         MyFrame frame = new MyFrame("Hello");  
+    }
+
+}
